a9a1031cf6660e431f5d3f075d6ad9d30016f685
Vitalii <job.corsar@gmail.com>
2018-09-06 19:25:55 +0300

    Home Work first commit

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _0903
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cockroach cockroach1 = new Cockroach(0, "#");
+            Cockroach cockroach2 = new Cockroach(1, "@");
+            Cockroach cockroach3 = new Cockroach(2, "&");
+            Cockroach cockroach4 = new Cockroach(3, "%");
+
+            new Thread(() => { cockroach1.Start(); }).Start();
+            new Thread(() => { cockroach2.Start(); }).Start();
+            new Thread(() => { cockroach3.Start(); }).Start();
+            new Thread(() => { cockroach4.Start(); }).Start();
+
+            Task.WaitAll();
+
+            Console.ReadKey();
+        }
+
+        public static void TaskThree()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+            var task1 = Task.Run(() => 
+            {
+                Thread.Sleep(1000);
+                cts.Cancel(true);
+            }, ct);
+
+            var task2 = Task.Run(() =>
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    Console.WriteLine("Hello world!!!");
+                }
+                Console.WriteLine("Stop");
+            });
+        }
+
+        public static void TaskOne()
+        {
+            var TaskReturnsInt = Task.Run(() =>
+            {
+                var rnd = new Random();
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+                return rnd.Next(1000);
+            });
+
+            var next = TaskReturnsInt.ContinueWith(x =>
+          {
+              Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+              Thread.Sleep(1000);
+              Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+              Console.WriteLine(x.Result * 10);
+              Console.WriteLine(x.Result);
+          });
+        }
+
+        public static void TaskTwo()
+        {
+            var TaskReturnString1 = Task.Run(() =>
+            {
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+                return "GENIA";
+            });
+
+            var TaskReturnString2 = TaskReturnString1.ContinueWith(x =>
+            {
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+                return "ILIA";
+            });
+
+            var TaskReturnString3 = TaskReturnString2.ContinueWith(x =>
+            {
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+                return "SASHA";
+            });
+
+            var TaskReturnString4 = TaskReturnString3.ContinueWith(x =>
+            {
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is running");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{ Thread.CurrentThread.ManagedThreadId} is finished");
+                Console.WriteLine($"KOLIA {TaskReturnString1.Result} {TaskReturnString2.Result} {TaskReturnString3.Result}");
+            });
+
+        }
+    }
+}
---
+++ cockroach.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace _0903
+{
+    public class Cockroach
+    {
+        private static int Finished { get; set; }
+        private int PositionColl { get; set; }
+        private int PositionLine { get; set; }
+        private string Name { get; set; }
+        private static CancellationTokenSource _cts;
+        private static CancellationToken _ct;
+        private static object _locker;
+
+        public Cockroach(int Number, string name)
+        {
+            _cts = new CancellationTokenSource();
+            _ct = _cts.Token;
+            _locker = new object();
+            PositionColl = 0;
+            Finished = 10;
+            PositionLine = Number;
+            Name = name;
+        }
+
+        private void Print()
+        {
+            lock (_locker)
+            {
+                Clear1();
+                Console.SetCursorPosition(PositionColl, PositionLine);
+                Console.Write(Name);
+            }
+        }
+
+        private void Clear1()
+        {
+            Console.SetCursorPosition(0, PositionLine);
+            Console.WriteLine("-----------|");
+        }
+
+        public void Start()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                Random rnd = new Random();
+
+                if (PositionColl == Finished)
+                {
+                    Console.SetCursorPosition(0, 5);
+                    Console.WriteLine($"{Name} finished first!!!");
+                    _cts.Cancel();
+                }
+
+                Print();
+
+                Thread.Sleep(rnd.Next(1000, 2000));
+                PositionColl++;
+            }
+        }
+    }
+}
